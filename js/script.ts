dc047719c33b0e8088875da22a9252eeb8fbcb91
Lumen <lumen.haendler@web.de>
2023-02-11 22:24:05 +0100

    Show character names on page

--- js/script.ts
+++ js/script.ts
@@ -6,21 +6,41 @@
         $.getJSON(apiURL, infodata => {
             for (let i = 1; i <= infodata.info.pages; i++) {
                 $.getJSON(apiURL + "?page=" + i, data => {
-                    console.log(i);
                     data.results.forEach((element: object) => {
-                        addCharacter(element);
+                        const charObj: object = createCharacterObject(element);
+                        addCharacter(charObj);
+                        showCharacter(charObj);
                     });
-                })
+                });
             }
         });
     }
 
-    function addCharacter(character) {
-        showCharacter(character.name)
+    function createCharacterObject(character) {
+        return {
+            name: character.name,
+            status: character.status,
+            species: character.species,
+            type: character.type,
+            gender: character.gender,
+            origin: character.origin.name,
+            location: character.location.name,
+            image: character.image
+        };
     }
 
-    function showCharacter(name) {
-        console.log(name);
+    function addCharacter(charObj: object) {
+        characters.push(charObj);
+    }
+
+    function showCharacter(charObj) {
+        let characterList = $(".character-list");
+        let characterListItem = $("<li class='character-list-item'></li>");
+        let characterButton = $("<button type='button' class='character-button'></button>");
+        characterButton.text(charObj.name);
+
+        characterListItem.append(characterButton);
+        characterList.append(characterListItem);
     }
 
     loadApiData();
